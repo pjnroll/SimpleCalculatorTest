3ea6275c66d83ee489eab364070c10e18d3a4c61
pjnroll <pjlaviano35@gmail.com>
2017-12-05 17:50:17 +0100

    Added instrumented test package

--- app/src/test/java/org/farfallettalaviano/simplecalculator/ProvaTestRule.java
+++ app/src/test/java/org/farfallettalaviano/simplecalculator/ProvaTestRule.java
@@ -3,11 +3,17 @@ package org.farfallettalaviano.simplecalculator;
 /**
  * Created by Stefano Farfalletta on 29/11/2017.
  */
+import org.junit.*;
+import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
 
+import static org.junit.Assert.assertEquals;
+
 @RunWith(JUnit4.class)
 public class ProvaTestRule {
-    //possiamo iniziare a testare le nostre fuznioni qui
-
+    @Test
+    public void addition_test() {
+        assertEquals(4, 2+2);
+    }
 }
